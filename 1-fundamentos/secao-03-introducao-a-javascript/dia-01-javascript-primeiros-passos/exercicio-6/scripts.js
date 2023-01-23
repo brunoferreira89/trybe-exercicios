5442de8d2782158d0652d2c3695d3188f7c9d622
Bruno Ferreira <bruno.ferreira89@hotmail.com>
2023-01-23 16:16:39 -0300

    Finalizado o exercicio 6 do dia 3.1

---
+++ 1-fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-6/scripts.js
@@ -0,0 +1,18 @@
+let statusCandidato = 'aprovado';
+
+switch (statusCandidato) {
+    case 'aprovado':
+        console.log ("Parabéns, você foi aprovada(o)!");
+        break;
+
+    case 'lista':
+        console.log ("Você está na nossa lista de espera.");
+        break;
+
+    case 'reprovado':
+        console.log ("Você foi reprovada(o)");
+        break;   
+
+    default:
+        console.log ("Informação incorreta.");
+}
