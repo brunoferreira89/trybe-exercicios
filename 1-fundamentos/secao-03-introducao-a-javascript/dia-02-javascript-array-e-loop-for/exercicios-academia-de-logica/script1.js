4ac8a1490dd0c4b147071601cd82fc6a12cd051d
Bruno Ferreira <bruno.ferreira89@hotmail.com>
2023-01-24 13:34:43 -0300

    Finalizado o exercicio 1 da Academia de Lógica

--- 1-fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicios-academia-de-logica/script1.js
+++ 1-fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicios-academia-de-logica/script1.js
@@ -1,12 +1,18 @@
 // Descubra a idade mínima
 
+// 1 - Atribuindo nomes a variáveis:
+
 let nome1 = 'Marina';
 let nome2 = 'Silvia';
 let nome3 = 'Iza';
 
+// 2 - Atribuindo idades a variáveis:
+
 let idade1 = 37;
 let idade2 = 48;
 let idade3 = 25;
+
+// 3 - Desenvolvendo o código:
 
 if (idade1 < idade2 && idade1 < idade3) {
     console.log(nome1 + ' é a pessoa mais jovem e possui ' + idade1 + ' anos de idade.');
