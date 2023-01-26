5d52c1d23c9b122860a14396d82315a763c3625e
Bruno Ferreira <bruno.ferreira89@hotmail.com>
2023-01-26 20:22:17 -0300

    Exercicio de leitura de objetos concluído

--- 1-fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicios-foguete/script2.js
+++ 1-fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicios-foguete/script2.js
@@ -1,7 +1,12 @@
 // Usando o objeto abaixo, faça os exercícios a seguir:
 
-// Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. Use a sintaxe meuObjeto.chave. Valor esperado no console: 
-// Bem-vinda, Margarida
+// Insira no objeto uma nova propriedade com o nome de chave ‘recorrente’ e o valor ‘Sim’ e, em seguida, imprima o objeto no console. Use a sintaxe meuObjeto['chave'] = valor. Valor esperado no console:
+// {
+//   personagem: 'Margarida',
+//   origem: 'Pato Donald',
+//   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   recorrente: 'Sim'
+// }
 
 
 let info = {
@@ -10,4 +15,5 @@ let info = {
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
   };
 
-  console.log('Bem-vinda, ' + info.personagem);
+  info['recorrente'] = 'Sim';
+  console.log(info);
---
+++ 1-fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicios-foguete/script3.js
@@ -0,0 +1,20 @@
+// Usando o objeto abaixo, faça os exercícios a seguir:
+
+// Faça um for/in que mostre todas as chaves do objeto. Valor esperado no console:
+// personagem
+// origem
+// nota
+// recorrente
+
+
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+  info['recorrente'] = 'Sim';
+
+for (let chave in info) {
+  console.log(chave);
+}
